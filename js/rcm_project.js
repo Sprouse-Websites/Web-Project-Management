12afa6030ee90672b83c5414b2343cc8e5769746
Joel Sprouse <joel.sprouse11@gmail.com>
2021-01-06 10:11:37 +0000

    Create rcm_project.js

---
+++ js/rcm_project.js
@@ -0,0 +1,12 @@
+var ProjectOptionList = document.getElementById("ProjectOptionList").style;
+
+function menu() {
+	ProjectOptionList.visibility = "visible";
+	ProjectOptionList.opacity = "1";
+	ProjectOptionList.left = "91pt";
+	ProjectOptionList.top = "87pt";
+}
+function clsmenu() {
+	ProjectOptionList.visibility = "hidden";
+	ProjectOptionList.opacity = "0";
+}
