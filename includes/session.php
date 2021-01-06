c9bb301fa02912059299ae1b6b9f414af9a827e5
Joel Sprouse <joel.sprouse11@gmail.com>
2021-01-06 10:12:02 +0000

    Update session.php
    Remove Old Code
    Refreshed Login code

--- includes/session.php
+++ includes/session.php
@@ -1,34 +1,16 @@
 <?php
-// include '../config.php';
-session_start();
-
-// if ($link -> connect_errno) {
-// 	echo "ERROR: Could not connect. " . mysqli_connect_errno() . mysqli_connect_error();
-// 	exit();
-// }
-
-// $link -> real_query("SELECT Username FROM users WHERE Username = '$user_check';");
-
-// if ($link -> field_count) {
-// 	$result = $link -> store_result();
-// 	$row = $result -> fetch_row();
-// 	$result -> free_result();
-// }
-
-// $link -> close();
 $URL = "$_SERVER[REQUEST_URI]";
-
-// function ForceLogin() {
-// 	echo "<script>window.location.href = '/webprojectmanagement/login.php?redirect=".$URL."';</script>";
-// }
 
 
 // echo $URL;
-if ($URL !== "/webprojectmanagement/login.php") {
-	if ($_SESSION['wpm_login_user'] == "") {
-		ForceLogin();
-	} elseif ($_SESSION['wpm_login_user'] == NULL) {
-		ForceLogin();
+if (strpos($URL, 'login.php') === false) {
+	echo "Not login page ";
+	if ($_SESSION['Username'] === NULL) {
+		echo "Not Logged in ";
+		echo "<script type=\"text/javascript\">
+		window.location.replace(\"https://www.webprojectmanagement.site/login.php\");
+		</script>";
+		exit;
 	}
 }
 
