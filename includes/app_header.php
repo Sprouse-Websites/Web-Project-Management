0a5035ba7dda70cd6e54403c2853dc60f2c228d8
Joel Sprouse <joel.sprouse11@gmail.com>
2021-01-06 10:12:46 +0000

    Update app_header.php
    Made links relative
    Renewed Login Buttons

--- includes/app_header.php
+++ includes/app_header.php
@@ -1,16 +1,23 @@
+<?php
+include 'https://www.webprojectmanagement.site/includes/config.php';
+include 'https://www.webprojectmanagement.site/includes/lang/index.php';
+?>
 <body>
 	<header class="w3-bar">
 		<div href="javascript:void(0);" class="w3-bar-item hidden">
-		<img src="/webprojectmanagement/favicon.ico" alt="" height="40pt">
+		<img src="../../favicon.ico" alt="" height="40pt">
 		<br>
 		WPM
 	</div>
-		<a href="/webprojectmanagement/app" class="w3-bar-item w3-button">Home</a>
-		<a href="/webprojectmanagement/app/projects" class="w3-bar-item w3-button">Projects</a>
-		<a href="/webprojectmanagement/app/settings" class="w3-bar-item w3-button">Settings</a>
+		<a href="/app" class="w3-bar-item w3-button"><?php echo $lang['Home']; ?><i class="fas fa-home"></i></a>
+		<a href="/app/projects" class="w3-bar-item w3-button"><?php echo $lang['Projects']; ?><i class="fas fa-tasks"></i></a>
+		<a href="/app/clients" class="w3-bar-item w3-button"><?php echo $lang['Clients']; ?><i class="fas fa-users"></i></a>
+		<a href="/app/settings" class="w3-bar-item w3-button"><?php echo $lang['Settings']; ?><i class="fas fa-cogs"></i></a>
 		<?php
-		if ($_SESSION['wpm_login_user'] !== NULL) {
-			echo "<a href=\"/webprojectmanagement/logout.php\" class=\"w3-bar-item w3-button\">Logout</a>";
+		if ($_SESSION['Username'] !== NULL) {
+			echo "<a href=\"/logout.php\" class=\"w3-bar-item w3-button\">" . $lang['Logout'] . "<i class=\"fas fa-sign-out-alt\"></i></a>";
+		} else {
+			echo "<a href=\"/login.php\" class=\"w3-bar-item w3-button\">" . $lang['Login'] . "<i class=\"fas fa-sign-in-alt\"></i></a>";
 		}
 		?>
 	</header>
