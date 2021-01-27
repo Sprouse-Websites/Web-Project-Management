1c18faa21f4216d46425e010ab9b916a3e0703f8
Joel Sprouse <joel.sprouse11@gmail.com>
2021-01-27 18:07:06 +0000

    Update index.php

--- app/projects/index.php
+++ app/projects/index.php
@@ -1,82 +1,202 @@
 <?php
 $page_name = "Projects | WPM";
+include '../../includes/privconfig.php';
 include '../../includes/config.php';
-include '../../includes/lang/index.php';
 include '../../includes/session.php';
 include '../../includes/head.php';
 include '../../includes/app_header.php';
 include '../../includes/auto-text-colour.php';
 include 'new.php';
 ?>
+<style media="screen">
+tr {
+	height: 40pt;
+}
+
+.progress-contatiner {
+    width: 100%;
+    background-color: #ddd;
+    text-align: center;
+    height: 40pt;
+    /* display: flex; */
+}
+
+.progress-bar {
+    color: white;
+    background-color: green;
+    height: 100%;
+}
+</style>
+<script src="../../js/tabs.js" charset="utf-8"></script>
 
 
-<div>
-	<?php
-	$sql = 'SELECT * FROM projects WHERE CompanyID = ' . $_SESSION['CompanyID'] . ' AND ProjectState = "Active";';
-	// echo $sql; // For Dedugging Purposes
-	if($result = mysqli_query($link, $sql)){
-		if(mysqli_num_rows($result) > 0){
-			echo "<table class=\"no-borders\" cellspacing=\"0\" cellpadding=\"0\" id=\"ProjectsTable\" width=\"100%\">";
-			echo "<thead>";
-			echo "<th onclick=\"sortTableByKey()\" title=\"Unique Project ID code\">Key</th>";
-			echo "<th onclick=\"sortTableByProject()\">Project Name</th>";
-			echo "<th>Description</th>";
-			echo "<th onclick=\"sortTableByClient()\">Client</th>";
-			echo "<th onclick=\"sortTableByProgress()\">Progress</th>";
-			echo "</thead>";
-			echo "<tbody>";
-			while($row = mysqli_fetch_array($result)){
-				$idInt = (int)$row['ProjectID'];
-				$Colour = $row['Colour'];
-				if ($row['Colour'] == NULL) {
-					$Colour = "white";
-					echo "<tr class=\"row-hover-opaque  auto-text-colour\" style=\"background-color:".$Colour.";\">";
-				} else {
-					echo "<tr class=\"row-hover-opaque  auto-text-colour\" style=\"background-color:".$Colour.";\">";
-				}
+<div class="tabs">
+	<div class="tab">
+		<button class="tablinks active" onclick="openTab(event, 'Active')" id="defaultOpen">Active</button>
+		<button class="tablinks" onclick="openTab(event, 'On Hold')">On Hold</button>
+		<button class="tablinks" onclick="openTab(event, 'Complete')">Complete</button>
+	</div>
 
-				echo "<td><a class=\"hidden\" href=\"../project?id=" . $idInt . "\">".$row['ProjectKey']."</a></td>";
-				echo "<td><a class=\"hidden\" href=\"../project?id=" . $idInt . "\">".$row['ProjectName']."</a></td>";
-				echo "<td><a class=\"hidden\" href=\"../project?id=" . $idInt . "\">".$row['Description']."</a></td>";
-				echo "<td><a class=\"hidden\" href=\"../project?id=" . $idInt . "\">".$row['Client']."</a></td>";
-				// FIXME: Progress not reciving sql
-				$sql2 = "SELECT SUM(TimeTracked) As TimeTrackedCnt, SUM(Duration) As DurationCnt FROM tasks WHERE ProjectId = ".$idInt;
-				if($result2 = mysqli_query($link, $sql)){
-					if(mysqli_num_rows($result2) > 0){
-						while($row2 = mysqli_fetch_array($result2)){
-							$TimeTracked = (float)$row2['TimeTrackedCnt'];
-							$Duration = (float)$row2['DurationCnt'];
-							$progress = $TimeTracked / $Duration;
-							$progress = $progress * 100;
-							if ($TimeTracked > $Duration) {
-								$var1 = "background-color:red !important; text-align:center;";
-							} else {
-								$var1 = "";
-							}
-						}
+
+	<div id="Active" class="tabcontent active">
+		<?php
+		$sql = 'SELECT * FROM projects WHERE CompanyID = ' . $_SESSION['CompanyID'] . ' AND ProjectState = "Active";';
+		// echo $sql; // For Dedugging Purposes
+		if($result = mysqli_query($link, $sql)){
+			if(mysqli_num_rows($result) > 0){
+				echo "<table class=\"no-borders\" cellspacing=\"0\" cellpadding=\"0\" id=\"ProjectsTable\" width=\"100%\">";
+				echo "<thead>";
+				echo "<th onclick=\"sortTableByKey()\" title=\"Unique Project ID code\">Key</th>";
+				echo "<th onclick=\"sortTableByProject()\">Project Name</th>";
+				echo "<th>Description</th>";
+				echo "<th onclick=\"sortTableByClient()\">Client</th>";
+				// echo "<th onclick=\"sortTableByProgress()\">Progress</th>";
+				echo "</thead>";
+				echo "<tbody>";
+				while($row = mysqli_fetch_array($result)){
+					$idInt = (int)$row['ProjectID'];
+					$Colour = $row['Colour'];
+					if ($row['Colour'] == NULL) {
+						$Colour = "white";
+						echo "<tr class=\"row-hover-opaque  auto-text-colour\" style=\"background-color:".$Colour.";\">";
+					} else {
+						echo "<tr class=\"row-hover-opaque  auto-text-colour\" style=\"background-color:".$Colour.";\">";
 					}
+
+					echo "<td><a class=\"hidden\" href=\"../project?id=" . $idInt . "\">".$row['ProjectKey']."</a></td>";
+					echo "<td><a class=\"hidden\" href=\"../project?id=" . $idInt . "\">".$row['ProjectName']."</a></td>";
+					echo "<td><a class=\"hidden\" href=\"../project?id=" . $idInt . "\">".$row['Description']."</a></td>";
+					echo "<td><a class=\"hidden\" href=\"../project?id=" . $idInt . "\">";
+					$sql3 = "SELECT * FROM clients WHERE ClientID = ".$row['ClientID'];
+					$result3 = mysqli_query($link, $sql3);
+					$client = mysqli_fetch_array($result3);
+					echo $client['Company'];
+					echo "</a></td>";
+
+					echo "</tr>";
 				}
-				echo "<td><a class=\"hidden\" href=\"../project?id=" . $idInt . "\"><div class=\"progress-contatiner\"><div class=\"progress-bar\" style=\"width:".(float)$progress."%; ".$var1."\">";
-
-				echo $TimeTracked;
-				echo "/";
-
-				echo $Duration;
-				echo"</div> </div></a></td>";
-				echo "</tr>";
+				echo "</tbody>";
+				echo "</table>";
 			}
-			echo "</tbody>";
-			echo "</table>";
+		} else {
+			echo "ERROR: Was not able to execute SQL: " . $sql . ".";
+			echo "<br/>";
+			echo mysqli_error($link);
+
+			echo (string)$link; // For Dedugging Purposes
 		}
-	} else {
-		echo "ERROR: Was not able to execute SQL: " . $sql . ".";
-		echo "<br/>";
-		echo mysqli_error($link);
 
-		echo (string)$link; // For Dedugging Purposes
-	}
+		?>
+	</div>
 
+	<script type="text/javascript">
+	openTab(event, 'Active');
+	</script>
 
+	<div id="On Hold" class="tabcontent">
+		<?php
+		$sql = 'SELECT * FROM projects WHERE CompanyID = ' . $_SESSION['CompanyID'] . ' AND ProjectState = "On Hold";';
+		// echo $sql; // For Dedugging Purposes
+		if($result = mysqli_query($link, $sql)){
+			if(mysqli_num_rows($result) > 0){
+				echo "<table class=\"no-borders\" cellspacing=\"0\" cellpadding=\"0\" id=\"ProjectsTable\" width=\"100%\">";
+				echo "<thead>";
+				echo "<th onclick=\"sortTableByKey()\" title=\"Unique Project ID code\">Key</th>";
+				echo "<th onclick=\"sortTableByProject()\">Project Name</th>";
+				echo "<th>Description</th>";
+				echo "<th onclick=\"sortTableByClient()\">Client</th>";
+				// echo "<th onclick=\"sortTableByProgress()\">Progress</th>";
+				echo "</thead>";
+				echo "<tbody>";
+				while($row = mysqli_fetch_array($result)){
+					$idInt = (int)$row['ProjectID'];
+					$Colour = $row['Colour'];
+					if ($row['Colour'] == NULL) {
+						$Colour = "white";
+						echo "<tr class=\"row-hover-opaque  auto-text-colour\" style=\"background-color:".$Colour.";\">";
+					} else {
+						echo "<tr class=\"row-hover-opaque  auto-text-colour\" style=\"background-color:".$Colour.";\">";
+					}
 
-	?>
+					echo "<td><a class=\"hidden\" href=\"../project?id=" . $idInt . "\">".$row['ProjectKey']."</a></td>";
+					echo "<td><a class=\"hidden\" href=\"../project?id=" . $idInt . "\">".$row['ProjectName']."</a></td>";
+					echo "<td><a class=\"hidden\" href=\"../project?id=" . $idInt . "\">".$row['Description']."</a></td>";
+					echo "<td><a class=\"hidden\" href=\"../project?id=" . $idInt . "\">";
+					$sql3 = "SELECT * FROM clients WHERE ClientID = ".$row['ClientID'];
+					$result3 = mysqli_query($link, $sql3);
+					$client = mysqli_fetch_array($result3);
+					if ($client['Company'] === NULL && $client['Company'] == "") {
+						echo $client['FirstName']." ".$client['LastName'];
+					} else {
+						$client['Company'];
+					}
+					echo "</a></td>";
+					echo "</tr>";
+				}
+				echo "</tbody>";
+				echo "</table>";
+			}
+		} else {
+			echo "ERROR: Was not able to execute SQL: " . $sql . ".";
+			echo "<br/>";
+			echo mysqli_error($link);
+
+			echo (string)$link; // For Dedugging Purposes
+		}
+
+		?>
+	</div>
+	<div id="Complete" class="tabcontent">
+		<?php
+		$sql = 'SELECT * FROM projects WHERE CompanyID = ' . $_SESSION['CompanyID'] . ' AND ProjectState = "Complete";';
+		// echo $sql; // For Dedugging Purposes
+		if($result = mysqli_query($link, $sql)){
+			if(mysqli_num_rows($result) > 0){
+				echo "<table class=\"no-borders\" cellspacing=\"0\" cellpadding=\"0\" id=\"ProjectsTable\" width=\"100%\">";
+				echo "<thead>";
+				echo "<th onclick=\"sortTableByKey()\" title=\"Unique Project ID code\">Key</th>";
+				echo "<th onclick=\"sortTableByProject()\">Project Name</th>";
+				echo "<th>Description</th>";
+				echo "<th onclick=\"sortTableByClient()\">Client</th>";
+				// echo "<th onclick=\"sortTableByProgress()\">Progress</th>";
+				echo "</thead>";
+				echo "<tbody>";
+				while($row = mysqli_fetch_array($result)){
+					$idInt = (int)$row['ProjectID'];
+					$Colour = $row['Colour'];
+					if ($row['Colour'] == NULL) {
+						$Colour = "white";
+						echo "<tr class=\"row-hover-opaque  auto-text-colour\" style=\"background-color:".$Colour.";\">";
+					} else {
+						echo "<tr class=\"row-hover-opaque  auto-text-colour\" style=\"background-color:".$Colour.";\">";
+					}
+
+					echo "<td><a class=\"hidden\" href=\"../project?id=" . $idInt . "\">".$row['ProjectKey']."</a></td>";
+					echo "<td><a class=\"hidden\" href=\"../project?id=" . $idInt . "\">".$row['ProjectName']."</a></td>";
+					echo "<td><a class=\"hidden\" href=\"../project?id=" . $idInt . "\">".$row['Description']."</a></td>";
+					echo "<td><a class=\"hidden\" href=\"../project?id=" . $idInt . "\">";
+					$sql3 = "SELECT * FROM clients WHERE ClientID = ".$row['ClientID'];
+					$result3 = mysqli_query($link, $sql3);
+					$client = mysqli_fetch_array($result3);
+					if ($client['Company'] === NULL && $client['Company'] == "") {
+						echo $client['FirstName']." ".$client['LastName'];
+					} else {
+						$client['Company'];
+					}
+					echo "</a></td>";
+					echo "</tr>";
+				}
+				echo "</tbody>";
+				echo "</table>";
+			}
+		} else {
+			echo "ERROR: Was not able to execute SQL: " . $sql . ".";
+			echo "<br/>";
+			echo mysqli_error($link);
+
+			echo (string)$link; // For Dedugging Purposes
+		}
+
+		?>
+	</div>
 </div>
