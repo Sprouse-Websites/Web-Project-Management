3f4eec47c45b5a0ccb5c28b0e5c0f151bb216f1c
Joel Sprouse <joel.sprouse11@gmail.com>
2021-01-27 17:57:47 +0000

    Update landing_header.php

--- includes/landing_header.php
+++ includes/landing_header.php
@@ -1,6 +1,6 @@
 <body>
 	<?php include '/includes/config.php';?>
-	<header class="w3-blue w3-bar">
+	<header class="w3-blue w3-bar" style="align-items:center;">
 		<a href="javascript:void(0);" class="w3-bar-item hidden">
 			<img src="/favicon.ico" alt="" height="30pt">
 			<br>
@@ -8,7 +8,7 @@
 		</a>
 		<a href="/" class="w3-bar-item w3-button">Home</a>
 		<a href="/about.php" class="w3-bar-item w3-button">About</a>
-		<a href="/login.php" class="w3-bar-item w3-button">Login</a>
+		<a href="/login.php" class="w3-bar-item w3-button">Login to the beta</a>
 	</header>
 
 	<main>
