53ef403dff2de517d5cf6fb90a34a0a1af28b2c4
Joel Sprouse <joel.sprouse11@gmail.com>
2021-01-27 17:58:30 +0000

    Update app_header.php

--- includes/app_header.php
+++ includes/app_header.php
@@ -1,25 +1,46 @@
 <?php
-include 'https://www.webprojectmanagement.site/includes/config.php';
-include 'https://www.webprojectmanagement.site/includes/lang/index.php';
+include 'includes/config.php';
 ?>
 <body>
 	<header class="w3-bar">
 		<div href="javascript:void(0);" class="w3-bar-item hidden">
-		<img src="../../favicon.ico" alt="" height="40pt">
-		<br>
-		WPM
-	</div>
-		<a href="/app" class="w3-bar-item w3-button"><?php echo $lang['Home']; ?><i class="fas fa-home"></i></a>
-		<a href="/app/projects" class="w3-bar-item w3-button"><?php echo $lang['Projects']; ?><i class="fas fa-tasks"></i></a>
-		<a href="/app/clients" class="w3-bar-item w3-button"><?php echo $lang['Clients']; ?><i class="fas fa-users"></i></a>
-		<a href="/app/settings" class="w3-bar-item w3-button"><?php echo $lang['Settings']; ?><i class="fas fa-cogs"></i></a>
-		<?php
-		if ($_SESSION['Username'] !== NULL) {
-			echo "<a href=\"/logout.php\" class=\"w3-bar-item w3-button\">" . $lang['Logout'] . "<i class=\"fas fa-sign-out-alt\"></i></a>";
-		} else {
-			echo "<a href=\"/login.php\" class=\"w3-bar-item w3-button\">" . $lang['Login'] . "<i class=\"fas fa-sign-in-alt\"></i></a>";
-		}
-		?>
+			<img src="../../favicon.ico" alt="" height="40pt">
+			<br>
+			WPM
+		</div>
+		<a href="/app" class="navbar-item">
+			<img src="/fa-icons/solid/home.svg" alt="<?php echo $lang['Home']; ?>" height="30px">
+		</a>
+		<a href="/app/projects" class="navbar-item">
+			<img src="/fa-icons/solid/tasks.svg" alt="<?php echo $lang['Projects']; ?>" height="30px">
+		</a>
+		<a href="/app/clients" class="navbar-item">
+			<img src="/fa-icons/solid/users.svg" alt="<?php echo $lang['Clients']; ?>" height="30px">
+		</a>
+		<a href="/app/organisation" class="navbar-item">
+			<img src="/fa-icons/solid/sitemap.svg" alt="<?php echo $lang['Organisation']; ?>" height="30px">
+		</a>
+		<div class="w3-dropdown-hover" style="right: 0; position: absolute;">
+			<div class="w3-button" style="font-size:16px;">
+				<?php echo $_SESSION['Username'] ?>
+				<img src="/fa-icons/solid/caret-down.svg" height="30px">
+			</div>
+			<div class="w3-dropdown-content w3-bar-block w3-card-4" style="right:0">
+				<a href="/app/settings" class="navbar-item">
+					<img src="/fa-icons/solid/cogs.svg" height="30px">
+					<?php echo $lang['Settings']; ?>
+				</a>
+				<?php
+				if ($_SESSION['Username'] !== NULL) {
+					echo "<a href=\"/logout.php?url=$URL\" class=\"navbar-item\"><img src=\"/fa-icons/solid/sign-out-alt.svg\" alt=\"\" height=\"30px\">" . $lang['Logout'] . "</a>";
+				} else {
+					echo "<a href=\"/login.php?url=$URL\" class=\"navbar-item\"><img src=\"/fa-icons/solid/sign-in-alt.svg\" alt=\"\" height=\"30px\">" . $lang['Login'] . "</a>";
+				}
+				?>
+			</div>
+		</div>
+
+
 	</header>
 
 	<main>
