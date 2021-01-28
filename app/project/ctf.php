d7b8cb761a8f8d41c9ee264ef7375cca234a8263
Joel Sprouse <joel.sprouse11@gmail.com>
2021-01-28 14:37:56 +0000

    Update ctf.php

--- app/project/ctf.php
+++ app/project/ctf.php
@@ -10,20 +10,4 @@ include '../../includes/app_header.php';
 	}
 
 
-if ($_POST[Duration] == NULL) {
-	$sql = "INSERT INTO `tasks` (`TaskName`, `Description`, `TaskPhase`, `CompanyID`, `ProjectId`) VALUES ('$_POST[task]', '$_POST[description]', '$_POST[TaskPhase]', $_SESSION[CompanyId], '$_POST[project]')";
-} else {
-	$sql = "INSERT INTO `tasks` (`TaskName`, `Description`, `TaskPhase`, `CompanyID`, `ProjectId`, `Duration`) VALUES ('$_POST[task]', '$_POST[description]', '$_POST[TaskPhase]', $_SESSION[CompanyId], '$_POST[project]', $_POST[Duration])";
-}
-
-
-
-if ($conn->query($sql) === TRUE) {
-		echo "New record created successfully";
-		header("Location: index.php?id=".$_POST[project]);
-} else {
-		// echo "We seem to have an issue. Please try calling instead and let us know you've had an issue. Sorry for any inconvenice.";
-		echo "Error: " . $sql . "<br>" . $conn->error;
-	}
-
 	$conn->close();
