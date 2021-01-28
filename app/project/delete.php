4aeb65ba8aa314a5108a9c5926a821045829b474
Joel Sprouse <joel.sprouse11@gmail.com>
2021-01-28 14:38:03 +0000

    Update delete.php

--- app/project/delete.php
+++ app/project/delete.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
-include '../includes/config.php';
-include '../includes/session.php';
-include '../includes/head.php';
-include '../includes/app_header.php';
+include '../../includes/config.php';
+include '../../includes/session.php';
+include '../../includes/head.php';
+include '../../includes/app_header.php';
 
 $_SESSION["project"] = $_POST["project"];
 $_SESSION["client"] = $_POST["client"];
@@ -14,29 +14,26 @@ $timestamp = date("Y-m-d H:i:s");
 // $comments = htmlentities($_SESSION['comments']);
 
 
-// Create connection
-$conn = mysqli_connect(db_server, db_username, db_password, db_database);
-
 // Check connection
 if ($conn->connect_error) {
-echo "Connection failed: " . $conn->connect_error;
+	echo "Connection failed: " . $conn->connect_error;
 }
 
-$sql = "INSERT INTO `projects` (`ProjectName`,`CompanyId`, `Client`,`Colour`,`DateCreated`,`DateModified`) VALUES ('{$_SESSION[project]}', 1, '{$_SESSION[client]}', '{$_SESSION[color]}', '$timestamp', '$timestamp')";
+$sql = "DELETE FROM `projects` WHERE `ProjectID` = ".$_GET['id'].";";
 
 
 
 if ($conn->query($sql) === TRUE) {
-echo "New Project added successfully";
-echo "<a href='index.php'>Go back</a>";
+	echo "Project deleted successfully";
+	echo "<a href='../projects'>Go back</a>";
 }
 else {
-echo "Data Failed to insert. Error Code: " . $sql . "<br>" . $conn->error;
+	echo "Data Failed to insert. Error Code: " . $sql . "<br>" . $conn->error;
 }
 
 $conn->close();
-include '../includes/footer.php';
-include '../includes/foot.php';
+include '../../includes/footer.php';
+include '../../includes/foot.php';
 
 
 ?>
